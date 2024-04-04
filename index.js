b5797b5aaa1497e5f599e533d8fe7d33aae7be8e
Julia Wey-Burgoyne <juliawey@gmail.com>
2024-04-04 19:24:25 -0400

    add completed lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const btn = document.querySelector('#button')
+    btn.addEventListener('click', handleClick)
+    function handleClick() {
+        alert("I was clicked!")
+    }
 }
